38d2f0c87d07baf384c382cdf91977634da9f48f
Lucas Lucas <kingfoxtroi@gmail.com>
2024-07-28 14:43:28 -0300

    Primeiro commit

---
+++ src/test/java/com/lucas/convertToPdf/ConvertToPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.convertToPdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertToPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
