6070b744682ee8c14752990ab91226a9466a934a
Lucas Lucas <kingfoxtroi@gmail.com>
2024-07-28 21:49:05 -0300

    Alterando o controller para retornar um responseEntity

--- src/main/java/com/lucas/convertToPdf/controllers/DocumentController.java
+++ src/main/java/com/lucas/convertToPdf/controllers/DocumentController.java
@@ -1,9 +1,11 @@
 package com.lucas.convertToPdf.controllers;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +23,15 @@ public class DocumentController {
 	private ConvertDocumentService documentService;
 
 	@PostMapping
-	public Map<String, Object> upload(@RequestParam MultipartFile file) throws IOException, DocumentException {
-		return documentService.upload(file);
+	public ResponseEntity<Map<String, Object>>  upload(@RequestParam MultipartFile file) throws IOException, DocumentException {
+		Map<String, Object> response = new HashMap<>();
+		
+		Integer statusCode = null;
+		
+		response = documentService.upload(file);
+		
+		statusCode = Integer.parseInt(response.get("status").toString());
+		
+		return ResponseEntity.status(statusCode).body(response);
 	}
 }
